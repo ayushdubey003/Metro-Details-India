6efe5f61ec5b5c412ab300849acb7e1fe91e1272
aysuhdubey003 <codeayush1998@gmail.com>
2019-06-11 13:24:33 +0530

    Add separators for metro rail ines

---
+++ Delhi Metro/modify1.py
@@ -0,0 +1,26 @@
+import csv
+
+readFile = open("DelhiMetroRouteInfo.csv", "r", newline='')
+l = list(readFile)
+
+r = []
+r.append("Route")
+writeFile = open("column1.csv", "a", newline='')
+f = csv.writer(writeFile)
+f.writerow([r[0]])
+
+for i in range(1, len(l)):
+    v = l[i].split(",")
+    route = v[4].split(":")
+    colors = v[8].split(":")
+    newRoute = colors[0] + "@" + route[0] + ":"
+    curr = 0
+    for j in range(1, len(route)):
+        if route[j].find("<") >= 0:
+            curr = curr + 1
+            newRoute = newRoute + route[j] + ";" + colors[curr] + "@"
+        else:
+            newRoute = newRoute + route[j] + ":"
+    f.writerow([newRoute])
+
+writeFile.close()
